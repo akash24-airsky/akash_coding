a6f8aa904cb577ccc59cf76777d68b5a6ab268f5
= <=>
2024-09-05 19:49:45 +0530

    Reverse string using StringBuffer

---
+++ src/com/string/Reverse_String_using_StringBuffer.java
@@ -0,0 +1,13 @@
+package com.string;
+
+public class Reverse_String_using_StringBuffer {
+    public static void main(String[] args) {
+
+        String rev = "hello";
+
+        System.out.println("original string is :"+revg);
+        StringBuffer sf = new StringBuffer(rev);
+
+        System.out.println("Reverse string is :"+sf.reverse());
+    }
+}
