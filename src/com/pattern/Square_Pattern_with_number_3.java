f97ebdb820d7831a13cd028c3d97b9e30436930d
= <=>
2024-09-06 09:30:22 +0530

    square pattern in vertical form

---
+++ src/com/pattern/Square_Pattern_with_number_3.java
@@ -0,0 +1,13 @@
+package com.pattern;
+
+public class Square_Pattern_with_number_3 {
+    public static void main(String[] args) {
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
